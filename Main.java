538536d23df99034283735f0b40c8f47bd208577
Taewoo Ki <80386626+taewooki@users.noreply.github.com>
2023-12-01 17:04:33 +0900

    If_03

--- Main.java
+++ Main.java
@@ -5,35 +5,30 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// 비교문, 조건문, Conditional Statement
-		Scanner	scanner = new Scanner(System.in);
-				
-		int num1 = 0;
-		int num2 = 0;
-		String result = ""; // 판단 결과 (문자열 생성)
 		
-		System.out.print("첫번째 숫자를 입력하세요 :");
-		num1 = scanner.nextInt();
-		System.out.print("첫번째 숫자를 입력하세요 :");
-		num2 = scanner.nextInt();
+		//문자 비교
+		
+		Scanner scanner = new Scanner(System.in);  // 2번째 화면
+		String id = "";
+		String password = "";
+		
+		String realId = "root";
+		String realPassword = "1234";
 		
 		
-		// 숫자 2개 받아오기
 		
+		System.out.println("** Log In Check **");  // 1번째 화면
 		
-		// num1이 num2보다 큰지 작은지 같은지 판단
-		if(num1 > num2)	{
-			result = "보다 큽니다.";
-			
-		} else if(num1 < num2 ){
-			result ="보다 작습니다.";
-			
+		System.out.println("ID를 입력하세요. :");    // 1번째 화면
+		id = scanner.next();
+		System.out.println("Password를 입력하세요 :");
+		password = scanner.next();
+		
+		if(realId.equals(id) && realPassword.equals(password)) {
+			System.out.println("환영 합니다!");
 		} else {
-			result ="와 같습니다.";
+			System.out.println("ID나 Password를 확인하세요!");
 		}
-		
-		
-		System.out.println(">>>>> End <<<<<<<");
 		
 
 	}
