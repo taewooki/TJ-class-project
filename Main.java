8aeef859bafc6b4a41614d1d44f3380e347efe0b
Taewoo Ki <80386626+taewooki@users.noreply.github.com>
2023-12-01 17:12:36 +0900

    Variable_03

--- Main.java
+++ Main.java
@@ -1,86 +1,30 @@
 package com.javalec.base;
 
+import java.util.Scanner;
+
 public class Main {
+
 	
 	public static void main(String[] args) {
-
-		//연산자 종류별 출력
-		
-		int num1 = 10;
-		int num2 = 2;
-		int num3 = 5;
-		
-		System.out.println("<<<산술 연산자>>>");
-		System.out.println("<술 연산자>>>");
-		System.out.println("<<<산술 연산자>>>");
-		
-		
-		System.out.println("<<<자동 증감 연산자>>>");
-	//	num1 = num1 + 1;
-	//	num1 += 1; 
-	//	덧셈으로 누적한다
-		num1++;
-		
-		System.out.println("증가 : "+ num1);
-	//	num1 - 1;
-	//	num1 -= 1;
-		num1--;
-		System.out.println("감소 : "+ num1);
-		
-		System.out.println("<<< 동등 비교 관계 연산자 >>>");
-		System.out.println("num1 == num2 :" + (num1==num2));
-		System.out.println("num2 != num3 :" + (num2!=num3));
-		System.out.println("num1 > num2 :" + (num1>num2));
-		System.out.println("num1 < num2 :" + (num1<num2));
-		System.out.println("num1 >= num2 :" + (num1>=num2));
-		System.out.println("num1 <= num2 :" + (num1<=num2));
-		
-		System.out.println("<<< 논리 연산자 >>>");
-		/*
-		 * true && true => true
-		 * true && false => false
-		 * false && true => false
-		 * false && false => false
-		 *
-		 * & and 조건
-		 */
-		
-		System.out.println("num1 > num2 && num2 > num3 :" + ((num1>num2) && (num2>num3)));
-		
-		/* or 조건
-		 * true || true => true
-		 * true || false => true
-		 * false || true => true
-		 * false || false => false
-		 */
-		System.out.println("num1 > num2 || num2 > num3 :" + ((num1>num2) || (num2>num3)));
-		
-		
-		System.out.println("<<< 삼항 연산자 >>>");
-		// 7이 3의 배수인지 판
-		System.out.println("7이 3의 배수인가? :" + (7 % 3 == 0 ? "3의배수" :"3의배수 아님"));
-		String result = (7 % 3==0 ? "3의배수" : "3의배수 아님");
-		System.out.println(result);
-		
-		
-		
-		String regiID = "abcd";
-		int regiPW = 1234;
-		
-		String inputID ="qwe";
-		int inputPW = 1234;
-		
-		System.out.println(regiID==inputID && regiPW==inputPW ?  "통과: 환영합니다" : "불통과 : id나 password를 다시 입력 하세요");
-		
-	
-				
 	
 		
+		// 사용자에게 입력받기! (정수)	
+		Scanner	scanner = new Scanner(System.in);
 		
 		
-		
-		
+	//	System.out.print("정수를 입력하세요 :");
+	//	int inputNum = scanner.nextInt();
+	//	System.out.print("입력된 숫자는" + inputNum + "입니다.")
+		;
 	
+		
+	// 사용자가 입력한 숫자가 짝수 인지 홀수 인지 출력하기
+		
+		System.out.print("정수를 입력하세요 :");
+		int inputNum2 = scanner.nextInt(); 
+		System.out.print( inputNum2%2==0? "짝수" : "홀수" );
+		
+		
 	}
 
 }
