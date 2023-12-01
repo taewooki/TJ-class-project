4e12c7389ed95da58a3f51b81250bf0806582646
Taewoo Ki <80386626+taewooki@users.noreply.github.com>
2023-12-01 17:14:47 +0900

    While_01

--- Main.java
+++ Main.java
@@ -1,37 +1,30 @@
 package com.javalec.base;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		// -----------------------------------------------------
-		// Desc : 사용자에게 정수를 입력 받아 짝수 인지 홀수 인지 판별한다.
-		// Date : 2023.11.23
-		// Author : Kenny
-		// ------------------------------------------------------
+		// 1 부터 100까지 합계 구하기
 		
+		// for(int i=1; i<=100; i++ ) {
+			
+	//	}
+		int i =1 ;
+		int tot = 0;
 		
-		//Property
-		Scanner scanner = new Scanner(System.in);
-		int inputNumber = 0; // 사용자에게 입력받은 정수
-		String result = ""; //짝수 인지 홀 수 인지 판단 결과
+	//	while(i <= 100) {
+	//		System.out.println(i);
+	//		i++;
+	//	}
+		while(true) {
+		//	System.out.println(i);
+			tot += i;
+			i++;
+			if(i>100) {
+				break;
+			}
+		}
+		System.out.println(tot);
 		
-		// Input		
-		System.out.println("정수를 입력하세요 :");
-		inputNumber = scanner.nextInt();
-		
-		// Process
-		
-		// System.out.println(inputNumber % 2 == 0 ? ("입력하신" + inputNumber + "는 짝수 입니다." )
-			//	: ("입력하신" + inputNumber + "는 홀수 입니다."));
-		
-		result = (inputNumber % 2 == 0 ? "짝수" : "홀수");
-		
-		// Output
-		System.out.println("입력하신" + inputNumber + "는(은) " + result + " 입니다.");
-		
-				
 		
 	}
 
