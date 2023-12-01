147d7676a3af010ad23abb76226ff3ef33c398b6
Taewoo Ki <80386626+taewooki@users.noreply.github.com>
2023-12-01 17:03:19 +0900

    If_01

--- Main.java
+++ Main.java
@@ -1,121 +1,41 @@
 package com.javalec.base;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// 구구단
-// -------------------		
-		// Scanner scanner= new Scanner(System.in);
-
-		// int dan = 0;
-
-		// System.out.println("구구단으로 사용할 숫자를 입력하세요!");
-		// dan = scanner.nextInt();
-
-// -------------------------------------------		
-		// for(int dan =2; dan <= 9; dan++) {
-		// System.out.println("-----------" + dan +"단 ---------");
-		// for(int i=1; i<=9; i++) {
-		// System.out.println( dan + " X " +i + " = " + (i*dan));
-
-		// ------------------------------------------------------
-
-		// }
-		// }
-
-		// System.out.print("1111111111\t");
-		// System.out.print("2222222222\t");
-		// System.out.print("3333333333\t");
-		// -------------------------------------
-
-		// for (int i=1; i<=9; i++) {
-		// for (int dan=2; dan<=9; dan++) {
-		// System.out.print(dan + " x " + i + " = " + (dan*i) + "\t");
-		// }
-		// System.out.println();
-
-		// }
-		// ---------------------------------------------------------
-
-		// for (int i=1; i<=9; i++) {
-		// for (int dan=2; dan<=9; dan++)
-		// if(i % 2 == 0)
-		// {
-		// System.out.print(dan + " x " + i + " = " + (i*dan) + "\t");
-
-		// }
-
-		// System.out.println();
-
-//	}
-
-		// ---------------------------------------------------------------------
-
-		// for (int i = 1; i <= 9; i++) {
-		// for (int dan = 2; dan <= 9; dan++) //nested for 문 , row
-		// if (i * dan % 2 == 0) { // column
-		// System.out.print(dan + " x " + i + " = " + (i * dan) + "\t");
-		// } else {
-		// System.out.print("\t\t"); // 띄어쓰기
-		// }
-		// System.out.println();
-		// }
-
-		// ------------------------------------------------------------------------------
-
-		// for (int i = 1; i <= 9; i++) {
-		// for (int dan = 2; dan <= 9; dan++)
-		// if (i % 2 == 0) {
-		// System.out.print(dan + " x " + i + " = " + (i * dan) + "\t");
-		// } else {
-		// System.out.print(dan + " x " + "* " + "= " + (i * dan) + "\t");
-		// }
-		// System.out.println();
-
-		// ---------------------------------------------------------------------------------
-
-		// for (int i = 1; i <= 9; i++) {
-		// for (int dan = 2; dan <= 9; dan++) {
-
-		// System.out.print(i % 2 == 0 ? dan + " x " + i + " = " + (i * dan) + "\t"
-		// : dan + " x " + "* " + "= " + (i * dan) + "\t");
-
-		// } // 삼항연산자는 print 문 안에 넣어 준다. 물어 볼때는 i % 2 == 0 ? a : b 로 물어본다.
-
-		// System.out.println();
-// --------------------------------------------------- 
-
-//		for (int i = 1; i <= 9; i++) {
-//			System.out.print(2 + " x " + i + " = " + (i * 2) + "\t");
-//			System.out.print(3 + " x " + i + " = " + (i * 3) + "\t");
-//			System.out.print(4 + " x " + i + " = " + (i * 4) + "\t");
-//			System.out.print(5 + " x " + i + " = " + (i * 5) + "\t");
-//			System.out.print(6 + " x " + i + " = " + (i * 6) + "\t");
-//			System.out.print(7 + " x " + i + " = " + (i * 7) + "\t");
-//			System.out.print(8 + " x " + i + " = " + (i * 8) + "\t");
-//			System.out.print(9 + " x " + i + " = " + (i * 9) + "\t");
-
-//			System.out.println();
-
-//		}
-//---------------------------------------------------------------------
-		int dan = 1;
-		if (dan >= 1 || dan < 9) {
-			dan++; }
-		{
-				System.out.print(dan);
+		// 비교문, 조건문, Conditional Statement
+		Scanner	scanner = new Scanner(System.in);
+				
+		int num1 = 0;
+		int num2 = 0;
+		String result = ""; // 판단 결과 (문자열 생성)
+		
+		System.out.print("첫번째 숫자를 입력하세요 :");
+		num1 = scanner.nextInt();
+		System.out.print("첫번째 숫자를 입력하세요 :");
+		num2 = scanner.nextInt();
+		
+		
+		// 숫자 2개 받아오기
+		
+		
+		// num1이 num2보다 큰지 작은지 같은지 판단
+		if(num1 > num2)	{
+			result = "보다 큽니다.";
 			
-		} //System.out.println(dan);
-		//{for (int i = 1; i <= 9; i++) {			
-		//	System.out.print(dan + " x " + i + " = " + (i * dan + "\t"));
-		//	{
-	
-		//		System.out.println();
+		} else if(num1 < num2 ){
+			result ="보다 작습니다.";
+			
+		} else {
+			result ="와 같습니다.";
+		}
+		
+		
+		System.out.println(">>>>> End <<<<<<<");
+		
 
-		//	}
-	//	}
-	// }
-}
-}
+	}
 
-// -----------------------------------------------------------------------
+}
--- Main2.java
+++ Main2.java
@@ -1,99 +1,45 @@
 package com.javalec.base;
 
+import java.util.Scanner;
+
 public class Main2 {
 
 	public static void main(String[] args) {
-		// 평균 구하기
+		// TODO Auto-generated method stub
 
-		// String[] str = {"a"."B"}; String[]
-		// int[] intNum = {1,2};
-		// double[] doubleNum = {1.2, 2.3};
-		// boolean[] boolNum = {true, false};
-
-		// -----------------------------
-
-		// 평균 구하기
-
-		// int[] intNum = { 1, 5, 8, 12, 200, 1002 };
-
-		// total 구하기
-
-		// double sum = 0; //변수 생성
-		// double aver = 0; // 변수 생성
-
-		// System.out.println(intNum.length);
-
-		// for (int i = 0; i < intNum.length; i++){
-		// sum += intNum[i];
-
-		// }
-		// aver = sum/intNum.length;
-
-		// System.out.println("합계 : " + sum +" / 평균 :"+aver);
-		// ------------------------------------------
-		// 평균 구하기
-		// int[] intNum = {30,30,100,100,200};
-
-		// total 구하기
-		// double sum=0; //변수 생성
-
-		// System.out.println(intNum.length);
-		// for (int i = 0; i< intNum.length; i++ ) {
-		// sum += intNum[i];
-		// }
-		// System.out.println(sum);
-
-		// 평균구하기
-
-		// System.out.println(sum/intNum.length);
-
-// ----------------------------------------------
-
-		// int[] intnum = { 100, 200, 300 };
-		// double sum = 0;
-		// 합계 구하기
-
-		// System.out.println(intnum.length);
-		// for (int i = 0; i < intnum.length; i++) {
-		// sum += intnum[i];
-		// }
-		// System.out.println(sum);
-
-		// 평균 구하기
-		// System.out.println(sum / intnum.length);
-
-		// -----------------------------------------
-
-		// --------------------------------------------------------------
-
-		// 배열 구구단 : 가로로 출력하기
-
-//		int[] danNum = {5,3,9};
-
-//		for (int i = 1; i<=9; i++) {
-//			for(int j=0; j < danNum.length; j++) { //배열에서 불러오는 값 만들어줌.
-
-//			System.out.print( danNum[j] + " x " + i + " = " + danNum[j]*i + "\t"); // 아님 System.out.println( danNum[1] + " x " + i + " = " + danNum[1]*i + "\t");
-		// 아님 System.out.println( danNum[2] + " x " + i + " = " + danNum[2]*i + "\t");
-
-//		}
-//		System.out.println();		// 칸을 나누어 
-
-		// ------------------------------------------------------
-
-		// 배열 구구단 : 가로로 출력하기
-
-		int[] danNum = { 2, 9, 3 };
-
-		for (int i = 1; i <= 9; i++) {
-			for (int j = 0; j < danNum.length; j++) {
-				System.out.print(danNum[j] + " x " + i + " = " + danNum[j] * i + "\t");
-			}
-
-			System.out.println();
+			// 비교문, 조건문, Conditional Statement
+			Scanner	scanner = new Scanner(System.in);
+					
+			int num1 = 0;
+						
+			String result = ""; // 판단 결과 (문자열 생성)
+			
+			System.out.print("금액을 입력하세요 :");
+			num1 = scanner.nextInt();
+						
+			// 숫자  받아오기
+			
+		
+			
+			// num1이 num2보다 큰지 작은지 같은지 판단
+			if(num1 <= 3000)	{
+				result = "싼편 입니다.";
+				
+			} else if(num1 > 3000 & num1 < 5000){
+				result ="적당한 금액 입니다.";
+				
+			} else if(num1 > 5000 & num1 <8000) {
+				result ="조금 비쌉니다.";
+				}
+			 else {
+			   result ="너무 비쌉니다.";
+		 }
+		
+			System.out.println(result);
+			System.out.println(">>>>> End <<<<<<<");
+			
 
 		}
-// -------------------------------------------------------------------------
 	}
 
-}
+
